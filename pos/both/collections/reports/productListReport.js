8cb60242ab953bff81efdb529bfce8322aa90938
phirum <phirumsim@gmail.com>
2016-05-17 15:57:20 +0700

    add product list report

---
+++ pos/both/collections/reports/productListReport.js
@@ -0,0 +1,13 @@
+Pos.Schema.ProductListReport = new SimpleSchema({
+    categoryId: {
+        type: String,
+        label: "Category",
+        autoform: {
+            type: "select2",
+            options: function () {
+                return Pos.List.category("All");
+            }
+        },
+        optional: true
+    }
+});
